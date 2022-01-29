476af0c455eb283ad160ca5b133624a5733de6cb
Marian Busila <mbusila@pfdrive.com>
2022-01-29 14:38:24 -0500

    create cdk project

---
+++ aws/cdk_net/sample-app/src/SampleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace SampleApp
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new SampleAppStack(app, "SampleAppStack");
+
+            app.Synth();
+        }
+    }
+}
